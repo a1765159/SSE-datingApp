3d8dae0e80289d8a3b3cd6d4af56f8b3a44e2d26
Justin Ngu <a1767346@student.adelaide.edu.au>
2022-11-05 16:36:52 +1030

    prints better log messages

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ var path = require('path')
 var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
 
 // setup the logger
-router.use(morgan('combined', { stream: accessLogStream }))
+router.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] - :response-time ms', { stream: accessLogStream }))
 
 // Welcome page
 router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
